b612bbdd7593a0171407b04dea01287a52f37891
KirillKulaev <139176905+KirillKulaev@users.noreply.github.com>
2023-07-12 23:06:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
-st.image(get_predict(uploaded_file))
+st.text(get_predict(uploaded_file))
