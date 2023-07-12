ad43dfc5133d50c87e4d9f3833ad833c69d7abfc
KirillKulaev <139176905+KirillKulaev@users.noreply.github.com>
2023-07-12 23:22:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import rdkit
+from rdkit.Chem import AllChem as Chem
+from rdkit.Chem import Draw
+from rdkit import DataStructs
 
 
 
 def get_predict(dataset):
   return 39.391830*dataset.loc[:, 'Mor28p:(MORDRED)'] + 26.446247*dataset.loc[:, 'Mor13se:(MORDRED)'] + 53.579802*dataset.loc[:, 'Mor26m:(MORDRED)'] - 0.330472*dataset.loc[:, 'PEOE_VSA7:(MORDRED)'] - 1.235251*dataset.loc[:, 'SsOH:(MORDRED)'] + 3.458921*dataset.loc[:, 'PEOE_VSA5:(MORDRED)'] - 336.598009*dataset.loc[:, 'GATS1d:(MORDRED)'] + 64.284856*dataset.loc[:, 'AATSC4dv:(MORDRED)'] + 350.2463171947596
 
+def smiles_visual(dataset):
+  data = get_predict(dataset)
+  mols = [Chem.MolFromSmiles(i) for i in dataset.loc[:, 'SMILES']]
+  return (Draw.MolsToGridImage(mols, legends = ['Pred.inhibition = '+str(np.array(i).round(2))+'%' for i in data]))
 
 st.title("QSAR prediction of inhibition of peroxidation oxidation")
 
@@ -25,4 +33,4 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
-st.write(get_predict(df))
+st.write(smiles_visual(df))
